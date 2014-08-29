2904bd9d0a858d7a39d243c8284c25dbfe35f00b
dominicalaus <dominicalaus@unknown68a86d52943c>
2014-08-28 20:22:56 -0500

    Added Interface page

---
+++ src/myinterface/Employee.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myinterface;
+
+/**
+ *
+ * @author dominicalaus
+ */
+public interface Employee {
+    public abstract void pay();
+    
+    public abstract int getAge();
+
+    public abstract void setAge(int age);
+
+    public abstract String getName();
+
+    public abstract void setName(String name);
+    
+    public abstract String getIdNumber();
+    
+    public abstract void setIdNumber(String IdNumber);
+}
